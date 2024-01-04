7fe779beecd235795b7a722d5618cf25b2fad796
Duc Thang <yourgitemail@example.com>
2024-01-04 17:25:44 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as csurf from 'csurf';
 import { AppModule } from './services/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+  app.use(csurf());
   await app.listen(process.env.PORT);
 }
 bootstrap();
